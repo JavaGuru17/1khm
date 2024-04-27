8cd8759b81b211d6e3f6c68e9c8ddf8c815c8ab3
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-04-27 13:33:03 +0500

    first commit

---
+++ src/test/java/uz/pdp/onekhm/ApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.onekhm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

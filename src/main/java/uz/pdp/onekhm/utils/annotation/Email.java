fff50d8c032db81d44e8153f59d65bf27d8c7833
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-04-28 13:48:42 +0500

    some problems fixed

--- src/main/java/uz/pdp/onekhm/utils/annotation/Email.java
+++ src/main/java/uz/pdp/onekhm/utils/annotation/Email.java
@@ -1,6 +1,7 @@
 package uz.pdp.onekhm.utils.annotation;
 
 import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 import uz.pdp.onekhm.utils.annotation.validation.EmailValidation;
 
 import java.lang.annotation.ElementType;
@@ -12,4 +13,9 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = EmailValidation.class)
 public @interface Email {
+    String message() default "";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
 }
--- src/main/java/uz/pdp/onekhm/utils/annotation/PhoneNumber.java
+++ src/main/java/uz/pdp/onekhm/utils/annotation/PhoneNumber.java
@@ -1,6 +1,7 @@
 package uz.pdp.onekhm.utils.annotation;
 
 import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
 import uz.pdp.onekhm.utils.annotation.validation.PhoneNumberValidation;
 
 import java.lang.annotation.ElementType;
@@ -12,4 +13,9 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = PhoneNumberValidation.class)
 public @interface PhoneNumber {
+    String message() default "";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
 }
